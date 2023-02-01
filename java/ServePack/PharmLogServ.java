8c63229dca2b43ad93d97bf82b40530f18fa668a
Jared Lundy <81462044+jared840@users.noreply.github.com>
2023-02-01 01:08:26 -0700

    Update PharmLogServ.java

--- java/ServePack/PharmLogServ.java
+++ java/ServePack/PharmLogServ.java
@@ -88,7 +88,7 @@ public class PharmLogServ extends HttpServlet {
 			//new as of 11-19:
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/medicalSystem", "root", "vick-newton7.1");
+			 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/medicalSystem", "root", "password");
 			
 			 String query="SELECT COUNT(*) FROM PHARMACIST WHERE username=?";
 				PreparedStatement prep=conn.prepareStatement(query);
