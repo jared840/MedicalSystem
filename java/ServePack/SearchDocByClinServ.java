f9538e3c5da3015ea6aa2b99fbf841d7e623def6
Jared Lundy <81462044+jared840@users.noreply.github.com>
2023-02-01 01:11:12 -0700

    Update SearchDocByClinServ.java

--- java/ServePack/SearchDocByClinServ.java
+++ java/ServePack/SearchDocByClinServ.java
@@ -45,7 +45,7 @@ public class SearchDocByClinServ extends HttpServlet {
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/medicalSystem", "root", "vick-newton7.1");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/medicalSystem", "root", "password");
 			
 			
 			
